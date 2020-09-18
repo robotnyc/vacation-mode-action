a91bc39c2692f93659478312cb4be85bdee3d059
Lucas Magasweran <lmagasweran@snapchat.com>
2020-09-18 23:07:43 +0200

    make async/await ready

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const core = require('@actions/core');
 const github = require('@actions/github');
+const core = require('@actions/core');
 
-try {
-  // `limit-group` input defined in action metadata file
-  const nameToGreet = core.getInput('limit-group');
-  console.log(`Hello ${nameToGreet}!`);
+async function run() {
+  try {
+    // `limit-group` input defined in action metadata file
+    const nameToGreet = core.getInput('limit-group');
+    console.log(`Hello ${nameToGreet}!`);
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 
-  // assuming issue event
-  const state = github.context.payload.issue.state;
-  console.log(`The : ${state}`);
-} catch (error) {
-  core.setFailed(error.message);
+    // assume issue event
+    const state = github.context.payload.issue.state;
+    console.log(`The : ${state}`);
+    if (state === "open") {
+
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
