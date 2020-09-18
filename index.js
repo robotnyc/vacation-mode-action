8015cd8cb1d00d3221b1719784fd7181e94895e9
Lucas Magasweran <lmagasweran@snapchat.com>
2020-09-18 23:27:58 +0200

    test getting restrictions

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ async function run() {
     if (state === "open") {
 
     }
+
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const myToken = core.getInput('myToken');
+
+    const octokit = github.getOctokit(myToken)
+
+    // You can also pass in additional options as a second parameter to getOctokit
+    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+
+    const { data: restrictions } = await octokit.interactions.getRestrictionsForRepo({
+        owner: github.repository_owner,
+        repo: github.repository_owner.split("/")[1],
+    });
+
+    console.log(restrictions);
   } catch (error) {
     core.setFailed(error.message);
   }
